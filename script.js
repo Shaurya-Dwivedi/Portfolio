2cb5661c835dcf9f896932bf7ebfc13df1a287a6
Shaurya-Dwivedi <shauryadwivedi2001@gmail.com>
2025-03-28 22:52:08 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+document.getElementById("welcome-body").addEventListener("mouseover", function (){
+    document.getElementById("welcome").classList.add("focus");
+})
+document.getElementById("welcome-body").addEventListener("mouseout", function (){
+    document.getElementById("welcome").classList.remove("focus");
+})
+
+document.getElementById("who-am-i-body").addEventListener("mouseover", function (){
+    document.getElementById("who-am-i").classList.add("focus");
+})
+document.getElementById("who-am-i-body").addEventListener("mouseout", function (){
+    document.getElementById("who-am-i").classList.remove("focus");
+})
+
+document.getElementById("what-i-do-body").addEventListener("mouseover", function (){
+    document.getElementById("what-i-do").classList.add("focus");
+})
+document.getElementById("what-i-do-body").addEventListener("mouseout", function (){
+    document.getElementById("what-i-do").classList.remove("focus");
+})
+
+document.getElementById("get-in-touch-body").addEventListener("mouseover", function (){
+    document.getElementById("get-in-touch").classList.add("focus");
+})
+document.getElementById("get-in-touch-body").addEventListener("mouseout", function (){
+    document.getElementById("get-in-touch").classList.remove("focus");
+})
